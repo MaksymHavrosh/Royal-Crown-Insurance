f672a4bab354528673c505e7fa61a60b2980b1bc
Maksym Havrosh <maks.havrosh99@gmail.com>
2020-06-02 12:23:00 +0300

    Refactor ServerManager

--- Royal Crown Insurance/Managers/ServerManager.swift
+++ Royal Crown Insurance/Managers/ServerManager.swift
@@ -16,41 +16,41 @@ class ServerManager {
     func getAboutRoyalAssist(success: @escaping (String) -> Void) {
         
         AF.request("http://31.131.21.105:82/api/v1/about_royal_assist",
-        method: .get,
-        encoding: URLEncoding.default,
-        headers: nil,
-        interceptor: nil).responseJSON { (response) in
-            
-            switch response.result {
-            case .success(let value):
-                guard let dict = value as? [String: Any], let text = dict["about_royal_assist"] as? String else { return }
-                
-                success(text)
-                
-            case .failure(let error):
-                print(error)
-            }
+                   method: .get,
+                   encoding: URLEncoding.default,
+                   headers: nil,
+                   interceptor: nil).responseJSON { (response) in
+                    
+                    switch response.result {
+                    case .success(let value):
+                        guard let dict = value as? [String: Any], let text = dict["about_royal_assist"] as? String else { return }
+                        
+                        success(text)
+                        
+                    case .failure(let error):
+                        print(error)
+                    }
         }
     }
     
     func getServices(params: [String : Any], success: @escaping ([[String : Any]]) -> Void) {
         
         AF.request("http://31.131.21.105:82/api/v1/services",
-        method: .get,
-        parameters: params,
-        encoding: URLEncoding.default,
-        headers: nil,
-        interceptor: nil).responseJSON { (response) in
-            
-            switch response.result {
-            case .success(let value):
-                guard let array = value as? [[String : Any]] else { return }
-                
-                success(array)
-                
-            case .failure(let error):
-                print(error)
-            }
+                   method: .get,
+                   parameters: params,
+                   encoding: URLEncoding.default,
+                   headers: nil,
+                   interceptor: nil).responseJSON { (response) in
+                    
+                    switch response.result {
+                    case .success(let value):
+                        guard let array = value as? [[String : Any]] else { return }
+                        
+                        success(array)
+                        
+                    case .failure(let error):
+                        print(error)
+                    }
         }
     }
     
